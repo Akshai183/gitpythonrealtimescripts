1b0a2db9b6b81993ae38eb8e88bc058879070966
Akshai183 <109257185+Akshai183@users.noreply.github.com>
2022-08-22 11:50:19 +0000

    Create initgit.sh

---
+++ initgit.sh
@@ -0,0 +1,7 @@
+echo "#Akshai" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git brnch -M main
+git remote add origin https://github.com/Akshai183/MyApp.git
+git push -u origin main
